0ce09fff453131276460c3c0a880ddaf71b845c7
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-08-17 17:18:01 -0500

    Add files via upload

---
+++ Heritability/Run_ADSP_Sex_Diff_REML.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+###Heritability Calcs -- GRM already made
+
+for i in NHW NHB; do
+for j in Men Women Interaction; do 
+for k in ALL Normals Impaired; do
+for l in MEM memslopes; do
+gcta64 --grm /data/h_vmac/eissmajm/Sex_Diff_ADSP_GWAS/GCTA/${i}/ADSP_${i}_${j}_${k}_Chrs1_22 --pheno /data/h_vmac/eissmajm/Sex_Diff_ADSP_GWAS/GCTA/${i}/ADSP_${i}_${l}_WithComorbidities_60plus.txt --mpheno 1 --reml --out /data/h_vmac/eissmajm/Sex_Diff_ADSP_GWAS/GCTA/${i}/ADSP_${i}_${j}_${k}_Chrs1_22_${l} --thread-num 10; done; done; done; done
