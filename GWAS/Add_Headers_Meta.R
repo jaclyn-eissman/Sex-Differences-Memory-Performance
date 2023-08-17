699129963b62bc39f54426d658dad53965fd160c
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-08-17 17:22:21 -0500

    Add files via upload

---
+++ GWAS/Add_Headers_Meta.R
@@ -0,0 +1,13 @@
+#Add back in headers to meta file
+library(data.table)
+args <- commandArgs(TRUE)
+meta_file <- args[1] 
+
+#Read in file
+file <- fread(meta_file,header=F)
+
+#Add in headers
+names(file) <- c("rs_number", "reference_allele", "other_allele", "eaf", "beta", "se", "beta_95L", "beta_95U", "z", "p-value", "-log10_p-value", "q_statistic", "q_p-value", "i2", "n_studies", "n_samples", "effects")
+
+#Write back out
+write.table(file,paste0(meta_file),quote=F,row.names=F,col.names=T,sep="\t")
---
+++ GWAS/Run_ADSP_Top_SNPs.sh
@@ -0,0 +1,18 @@
+###Set to fail if error
+set -e 
+
+###NHW and NHB
+for i in Men Women Interaction; do
+for j in MEM memslopes; do
+for k in WithComorbidities WithoutComorbidities; do
+for l in ALL Normals Impaired; do
+for m in NHW NHB; do
+Rscript /data/h_vmac/eissmajm/Sex_Diff_ADSP_GWAS/Slurm/Get_Top_SNPs.R /data/h_vmac/eissmajm/Sex_Diff_ADSP_GWAS/GWAS/${m} ADSP_${m}_${j}_${k}_60plus_${i}_${l}_subset_MAF01_edited_withX.out; done; done; done; done; done
+
+
+###Cross-Ancestry
+for i in Men Women Interaction; do
+for j in MEM memslopes; do
+for k in WithComorbidities WithoutComorbidities; do
+for l in ALL Normals Impaired; do
+Rscript /data/h_vmac/eissmajm/Sex_Diff_ADSP_GWAS/Slurm/Get_Top_SNPs.R /data/h_vmac/eissmajm/Sex_Diff_ADSP_GWAS/GWAS/Cross_Ancestry ADSP_Cross_Ancestry_${j}_${k}_60plus_${i}_${l}_subset_edited_withX.out; done; done; done; done
