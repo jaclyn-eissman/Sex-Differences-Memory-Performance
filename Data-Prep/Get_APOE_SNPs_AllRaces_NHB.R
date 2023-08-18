0b71c05668743e046ede00f00518e1d17c4c685b
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-08-18 11:36:29 -0500

    Update Get_APOE_SNPs_AllRaces_NHB.R

--- Data-Prep/Get_APOE_SNPs_AllRaces_NHB.R
+++ Data-Prep/Get_APOE_SNPs_AllRaces_NHB.R
@@ -5,16 +5,16 @@
 #libraries
 library(data.table)
 
-#NHB
+#Read in bim file with SNP, Chr, and BP information
 bim <- fread("/data/h_vmac/eissmajm/Sex_Diff_ADSP_GWAS/Data/NHB/ADSP_NHB.bim") 
-bim_chr19 <- bim[bim$V1==19 & bim$V4>=43905796 & bim$V4<=45909395,]
+bim_chr19 <- bim[bim$V1==19 & bim$V4>=43905796 & bim$V4<=45909395,] #select a 1Mb region around APOE
 bim_chr19_rs <- bim_chr19$V2
 write.table(bim_chr19_rs,"/data/h_vmac/eissmajm/Sex_Diff_ADSP_GWAS/Data/NHB/NHB_No_Relateds_Merged_First_Pass_APOE_1Mb_rs.txt",quote=F,col.names=F,row.names=F)
 rm(bim,bim_chr19,bim_chr19_rs)
 
-#All Races
+#Read in bim file with SNP, Chr, and BP information
 bim <- fread("/data/h_vmac/eissmajm/Sex_Diff_ADSP_GWAS/Data/NHB/No_Relateds_Merged_First_Pass.bim") 
-bim_chr19 <- bim[bim$V1==19 & bim$V4>=43905796 & bim$V4<=45909395,]
+bim_chr19 <- bim[bim$V1==19 & bim$V4>=43905796 & bim$V4<=45909395,] #select a 1Mb region around APOE
 bim_chr19_rs <- bim_chr19$V2
 write.table(bim_chr19_rs,"/data/h_vmac/eissmajm/Sex_Diff_ADSP_GWAS/Data/NHB/AllRaces_No_Relateds_Merged_First_Pass_APOE_1Mb_rs.txt",quote=F,col.names=F,row.names=F)
 rm(bim,bim_chr19,bim_chr19_rs)
