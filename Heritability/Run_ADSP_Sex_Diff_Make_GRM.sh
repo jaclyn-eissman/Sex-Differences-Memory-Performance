f1fdbcdfb7a24636bd178d6a1f62d72754499fe5
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-08-18 11:58:01 -0500

    Update Run_ADSP_Sex_Diff_Make_GRM.sh

--- Heritability/Run_ADSP_Sex_Diff_Make_GRM.sh
+++ Heritability/Run_ADSP_Sex_Diff_Make_GRM.sh
@@ -6,5 +6,5 @@ race=$1
 analysis=$2
 dx=$3
 
-#Build relatedness matrix
+#Build genetic relatedness matrix (GRM)
 gcta64 --bfile /data/h_vmac/eissmajm/Sex_Diff_ADSP_GWAS/GCTA/${race}/ADSP_${race}_${analysis}_${dx}_Second_Pass_geno05 --make-grm --out /data/h_vmac/eissmajm/Sex_Diff_ADSP_GWAS/GCTA/${race}/ADSP_${race}_${analysis}_${dx}_Chrs1_22 --thread-num 10
